20537a9b293aa599e04d4ac44e2ac267c5767338
UVRay <UVRay@users.noreply.github.com>
2025-06-30 20:58:05 +0100

    Change metadata host address from api.lidarr.audio to api.musicinfo.pro

--- src/NzbDrone.Common/Cloud/LidarrCloudRequestBuilder.cs
+++ src/NzbDrone.Common/Cloud/LidarrCloudRequestBuilder.cs
@@ -16,7 +16,7 @@ namespace NzbDrone.Common.Cloud
             Services = new HttpRequestBuilder("https://lidarr.servarr.com/v1/")
                 .CreateFactory();
 
-            Search = new HttpRequestBuilder("https://api.lidarr.audio/api/v0.4/{route}")
+            Search = new HttpRequestBuilder("https://api.musicinfo.pro/api/v0.4/{route}")
                 .KeepAlive()
                 .CreateFactory();
         }
